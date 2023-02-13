2e037c64c55ab49b8d60162762aee608b632dae5
patrickdemurtas <patrickmurtass@gmail.com>
2023-02-13 05:58:25 -0300

    turning back: rentals

--- src/controllers/rentalControllers.js
+++ src/controllers/rentalControllers.js
@@ -35,20 +35,20 @@ export async function insertRent(req, res) {
 
     try {
 
-        const game = await db.query("SELECT * FROM games WHERE id = $1", [gameId]);
+        const game = await db.query(`SELECT * FROM ${gameTab} WHERE id = $1`, [gameId]);
         if (!game.rows.length) {
             return res.sendStatus(400);
         }
         originalPrice = game.rows[0].pricePerDay * daysRented;
 
 
-        const customer = await db.query("SELECT * FROM customers WHERE id = $1", [customerId]);
+        const customer = await db.query(`SELECT * FROM ${cusTab} WHERE id = $1`, [customerId]);
         if (!customer.rows.length) {
             return res.sendStatus(400)
         }
 
 
-        const rentedGames = await db.query("SELECT * FROM rentals WHERE gameId = $1 AND returnDate IS NULL", [gameId]);
+        const rentedGames = await db.query(`SELECT * FROM ${renTab} WHERE "gameId" = $1 AND "returnDate" IS NULL`, [gameId]);
         const totalStock = game.rows[0].stockTotal;
         const rentedStock = rentedGames.rows.length;
         if (totalStock <= rentedStock) {
@@ -57,7 +57,7 @@ export async function insertRent(req, res) {
 
 
         const rental = await db.query(
-            "INSERT INTO rentals (customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee) VALUES ($1, $2, $3, $4, $5, $6, $7);",
+            `INSERT INTO ${renTab} ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7);`,
             [customerId, gameId, rentalDate, daysRented, returnDate, originalPrice, lateFee]
         );
         res.sendStatus(201);
@@ -67,14 +67,15 @@ export async function insertRent(req, res) {
 }
 
 
-export async function listRentals(req, res) {
+
+export async function fetchRentals(req, res) {
     try {
-    const queryResult = await db.query(
-        `WITH join_table AS ( SELECT rentals.id, rentals."customerId", rentals."gameId", rentals."rentDate", rentals."daysRented", rentals."returnDate", rentals."originalPrice", rentals."delayFee", customers.id AS customer_id, customers.name AS customer_name, games.id AS game_id, games.name AS game_name FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id ) SELECT * FROM ( SELECT id, "customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee", json_build_object('id', customer_id, 'name', customer_name) AS customer, json_build_object('id', game_id, 'name', game_name) AS game FROM join_table ) join_table; `
+        const queryResult = await db.query(
+            `WITH join_table AS ( SELECT rentals.id, rentals."customerId", rentals."gameId", rentals."rentDate", rentals."daysRented", rentals."returnDate", rentals."originalPrice", rentals."delayFee", customers.id AS customer_id, customers.name AS customer_name, games.id AS game_id, games.name AS game_name FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id ) SELECT * FROM ( SELECT id, "customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee", json_build_object('id', customer_id, 'name', customer_name) AS customer, json_build_object('id', game_id, 'name', game_name) AS game FROM join_table ) join_table; `
         );
-    console.log(queryResult.rows);
-res.status(200).send(queryResult.rows);
-} catch (error) {
-    res.sendStatus(500);
+        console.log(queryResult.rows);
+        res.status(200).send(queryResult.rows);
+    } catch (error) {
+        res.sendStatus(500);
     }
-    }
+}
